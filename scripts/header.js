108f58df1d46ad9475555263a8d28c97cc9bbd80
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2025-01-09 23:35:42 -0600

    fix for coordinates page. add coming soon text

--- scripts/header.js
+++ scripts/header.js
@@ -10,7 +10,7 @@ function setSelectedButton() {
     const path = window.parent.location.pathname;
     if (path == "/") {
         document.querySelector('.navButton.home').classList.add('selected');
-    } else if (path == "/coordinates") {
+    } else if (path == "/coordinates/") {
         document.querySelector('.navButton.coordinates').classList.add('selected');
     }
 }
