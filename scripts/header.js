2a9b3b103266595171eaf6b16afce8e8d56812b9
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2025-01-09 23:33:59 -0600

    button class automation

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,18 @@
 function toHome() {
-    window.parent.location.assign("/")
+    window.parent.location.assign("/");
 }
 
 function toCoordinates() {
-    window.parent.location.assign("/coordinates")
+    window.parent.location.assign("/coordinates");
 }
+
+function setSelectedButton() {
+    const path = window.parent.location.pathname;
+    if (path == "/") {
+        document.querySelector('.navButton.home').classList.add('selected');
+    } else if (path == "/coordinates") {
+        document.querySelector('.navButton.coordinates').classList.add('selected');
+    }
+}
+
+setSelectedButton();
